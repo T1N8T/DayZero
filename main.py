b8c8003f0974c9d6c0afa1842430eccd33fc51a4
Torrente <torrentep18@gmail.com>
2025-03-24 16:48:38 +0100

    cargar los datos del ejemplo

--- main.py
+++ main.py
@@ -4,12 +4,25 @@ import folium
 from pyproj import Geod
 
 
-
+    
 
 def main():
-    """Punto de entrada principal del programa."""
-    print("Hola, mundo!")
-    return True
+    # Load the data
+    predictions = pd.read_csv('samples/sample_predictions_empty.csv')
+    
+    predictions_vector = []
+
+    for _, linea in predictions.iterrows():
+        predictions_vector.append(linea.tolist())
+        
+        ID_SCENARIO = predictions_vector[0]
+        ICAO = predictions_vector[1]
+        RUNWAY = predictions_vector[2]
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
