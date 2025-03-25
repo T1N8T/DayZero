1d0fb3e36bb1a0a6cb20042ebe24d716f2feff0d
Torrente <torrentep18@gmail.com>
2025-03-25 11:14:45 +0100

    por fin da algo decente

--- main.py
+++ main.py
@@ -24,14 +24,18 @@ def write_results(prediccion, ID_SCENARIO, ICAO, RUNWAY):
         # Si el archivo existe, agrega los datos sin sobrescribir encabezados
         results.to_csv(file_path, mode='a', index=False, header=False)
 
-def calc_prediction(lat_deg,lon_deg,coordinates_lat,coordinates_lon,air_speed):
+def calc_prediction(lat_deg,lon_deg,coordinates_lat,coordinates_lon,ground_speed,altitude):
     Radius = float(6371000)
     dlat = math.radians(coordinates_lat - lat_deg)
     dlon = math.radians(coordinates_lon - lon_deg)
-    a = math.sin(dlat/2)^2 + math.cos(math.radians(lat_deg)) * math.cos(math.radians(coordinates_lat)) * math.sin(dlon/2)^2
+    a = math.sin(dlat/2)**2 + math.cos(math.radians(lat_deg)) * math.cos(math.radians(coordinates_lat)) * math.sin(dlon/2)**2
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-    s = Radius * c / 1000;
-    return s/air_speed*3600  #Convert distance to kilometers
+    distancia = (Radius * c)/1000  # Distance in kilometers
+
+    # Convert ground speed from knots (nautical miles per hour) to kilometers per hour
+    ground_speed = ground_speed * 1.852
+    ground_speed = (ground_speed + 220)/2 
+    return distancia/ground_speed * 3600 
     
 
 def main():
@@ -56,11 +60,10 @@ def main():
         data = data[data['icao24'] == ICAO]
         data["datetime"] = pd.to_datetime(data["ts"], unit="ms")
 
-        columns = ['icao24','datetime','lat_deg','lon_deg','bds60_IAS']
+        columns = ['icao24','datetime','lat_deg','lon_deg','altitude','groundspeed']
         data = data[columns]
-        data = data.ffill()
-        
-        
+        data['groundspeed'] = data['groundspeed'].interpolate(method='linear')
+        data['altitude'] = data['altitude'].interpolate(method= 'linear')        
         # Get the last data
         data = data[data["datetime"] == data["datetime"].max()]
 
@@ -69,19 +72,21 @@ def main():
         long_deg = data.iloc[0].lon_deg
 
         #Get the speed of the aircraft
-        air_speed = data.iloc[0].bds60_IAS
+        ground_speed = data.iloc[0].groundspeed
 
+        #Get the last altitude
+        altitude = data.iloc[0].altitude
 
         #Get the coordinates of the runway
         runway_data = thresholds[thresholds['runway'] == RUNWAY]
         coordinates = runway_data.iloc[0].geometry.coords[0]
-        runway_lat = coordinates[0]
-        runway_lon = coordinates[1]
+        runway_lon = coordinates[0]
+        runway_lat = coordinates[1]
 
         
         #Calculate the time in seconds
-        print (lat_deg, long_deg, runway_lat, runway_lon,air_speed)
-        prediction = calc_prediction(lat_deg, long_deg, runway_lat, runway_lon,air_speed) 
+        print (lat_deg, long_deg, runway_lat,runway_lon,ground_speed,altitude)
+        prediction = calc_prediction(lat_deg, long_deg, runway_lat, runway_lon,ground_speed,altitude) 
         # Write the results
         write_results(prediction, ID_SCENARIO, ICAO, RUNWAY)
 
